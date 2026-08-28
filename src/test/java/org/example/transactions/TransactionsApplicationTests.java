fb24b4ae3e346e3647df89f3dfc43917dcfc8fec
suyash giri <suyashgiri938@gmail.com>
2026-08-28 07:57:43 +0530

    Transactions Project Initialized

---
+++ src/test/java/org/example/transactions/TransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
